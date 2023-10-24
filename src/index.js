6026d300693e11db2b071ee3a0fd5788d77e510a
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-10-24 17:21:30 +1100

    index.js initialized and basic consts added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log("hello")
+
+const url = "https://dog.ceo/api/breeds/list/all/"
+const dogCard = document.getElementById("dog-card")
+const button = document.getElementById("btn")
+
+let findDog = () => {
+    let id = url
+}
+
+button.addEventListener("click", findDog);
+
