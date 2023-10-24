0a720cdb72c41047490bac14f74031a7fb797aaf
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-10-24 18:12:26 +1100

    changes style.css/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 console.log("hello")
 
-const url = "https://dog.ceo/api/breeds/list/all/"
+const url = "https://dog.ceo/api/breeds/image/random"
 const dogCard = document.getElementById("dog-card")
 const button = document.getElementById("btn")
 
-let findDog = () => {
-    let id = url
-}
+fetch(url)
+    .then(resp => resp.json())
+    .then((data) => {
+        // generateDog(data)
+        console.log(data)
+    })
 
 button.addEventListener("click", findDog);
 
