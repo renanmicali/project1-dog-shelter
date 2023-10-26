07c1949d209b120b088288b4e20272327c0f8630
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-10-26 12:58:34 +1100

    final img  URL

--- src/index.js
+++ src/index.js
@@ -11,18 +11,26 @@ const img = document.getElementById("dog-pic")
 
 let findDog = () => {
 
-fetch(url)
-    .then(resp => resp.json())
-    .then((data) => {
-        let dogArr = Object.keys(data.message)
-        console.log(dogArr[randomBreedIndex()])
-    })
+    fetch(url)
+        .then(resp => resp.json())
+        .then((data) => {
+            let dogArr = Object.keys(data.message)
+
+            const finalUrl = `https://dog.ceo/api/breed/${dogArr[randomBreedIndex()]}/images/random`
+
+            //fetching final URL for img
+            fetch(finalUrl)
+                .then(resp => resp.json())
+                .then(data => {
+                    console.log(data.message)
+                })
+        })
 }
 
 button.addEventListener("click", findDog);
 
 
 // random number for dog array 
-function randomBreedIndex () {
+function randomBreedIndex() {
     return Math.floor(Math.random() * 99)
 }
