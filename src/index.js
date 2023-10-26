4579e6916f9512ba3bb4041092cf385d8eb94160
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-10-26 12:37:43 +1100

    dog array creation and random number

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 console.log("hello")
 
-const url = "https://dog.ceo/api/breeds/image/random"
+const url = "https://dog.ceo/api/breeds/list/all"
 const dogCard = document.getElementById("dog-card")
 const button = document.getElementById("btn")
+const img = document.getElementById("dog-pic")
+
+// looping thourgh array to get a radnon index number
+// retrive the data
+// const test = "https://dog.ceo/api/breed/${loop here}/images/random"
+
+let findDog = () => {
 
 fetch(url)
     .then(resp => resp.json())
     .then((data) => {
-        // generateDog(data)
-        console.log(data)
+        let dogArr = Object.keys(data.message)
+        console.log(dogArr[randomBreedIndex()])
     })
+}
 
 button.addEventListener("click", findDog);
 
+
+// random number for dog array 
+function randomBreedIndex () {
+    return Math.floor(Math.random() * 99)
+}
