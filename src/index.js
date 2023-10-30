2ce98da18f1ffbdaeaaf14dbdc23a3413e66006e
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-10-30 18:20:55 +1100

    javascript updated/changing app

--- src/index.js
+++ src/index.js
@@ -4,33 +4,37 @@ const url = "https://dog.ceo/api/breeds/list/all"
 const dogCard = document.getElementById("dog-card")
 const button = document.getElementById("btn")
 const img = document.getElementById("dog-pic")
+const card = document.getElementsByClassName("container")
+const form = document.querySelector('form')
+const yourDog = document.getElementById('your-dog')
+const list = document.getElementById('breed')
+console.log(img)
 
-// looping thourgh array to get a radnon index number
-// retrive the data
-// const test = "https://dog.ceo/api/breed/${loop here}/images/random"
+fetch(url)
+    .then(resp => resp.json())
+    .then((data) => {
+        createBreedList(data.message)
+     
+       
+    })
 
-let findDog = () => {
-
-    fetch(url)
-        .then(resp => resp.json())
-        .then((data) => {
-            let dogArr = Object.keys(data.message)
-
-            const finalUrl = `https://dog.ceo/api/breed/${dogArr[randomBreedIndex()]}/images/random`
-
-            //fetching final URL for img
-            fetch(finalUrl)
-                .then(resp => resp.json())
-                .then(data => {
-                    console.log(data.message)
-                })
-        })
+function createBreedList (breedList) {
+    // 'this' will point to element in question
+    list.innerHTML =`
+            <select onchange="loadBreed(this.value)" name="" id="">
+            <option>Choose a Dog Breed To Adopt</option>
+            ${Object.keys(breedList).map((breed) => {
+                return `<option>${breed}</option>`
+            }).join('')}
+       </select>`
 }
-
-button.addEventListener("click", findDog);
-
-
-// random number for dog array 
-function randomBreedIndex() {
-    return Math.floor(Math.random() * 99)
+// londing images by breed
+function loadBreed(breed) {
+    if (breed != "Choose a Dog Breed To Adopt") {
+        fetch(`https://dog.ceo/api/breed/${breed}/images`)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
 }
