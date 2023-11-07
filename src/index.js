e54572f15f217e788d9ba062753360502a6147a2
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-11-07 20:39:16 +1100

    changes js

--- src/index.js
+++ src/index.js
@@ -1,53 +1,76 @@
 console.log("hello")
 
 const url = "https://dog.ceo/api/breeds/list/all"
-const dogCard = document.getElementById("dog-card")
-const button = document.getElementById("btn")
-const img = document.getElementById("dog-pic")
-const card = document.getElementsByClassName("container")
-const form = document.querySelector('form')
-const yourDog = document.getElementById('your-dog')
 const list = document.getElementById('breed')
-console.log(img)
+
 
 fetch(url)
     .then(resp => resp.json())
     .then((data) => {
         createBreedList(data.message)
-     
-       
-    })
 
-function createBreedList (breedList) {
-    // 'this' will point to element in question
-    list.innerHTML =`
-            <select onchange="loadBreed(this.value)" name="" id="">
+
+    })
+// using map to show list and allow all breeds
+function createBreedList(breedList) {
+    // !! 'this' will point to element in question when no other parameter can be used !!
+    // using on change event to select breed
+    list.innerHTML = `
+            <select onchange="loadBreed(this.value)" name="" id="opt">
             <option>Choose a Dog Breed To Adopt</option>
             ${Object.keys(breedList).map((breed) => {
-                return `<option>${breed}</option>`
-            }).join('')}
+        return `<option>${breed}</option>`
+    }).join('')}
             </select>`
 }
 // londing images by breed
 function loadBreed(breed) {
     if (breed != "Choose a Dog Breed To Adopt") {
         fetch(`https://dog.ceo/api/breed/${breed}/images`)
-        .then(resp => resp.json())
-        .then(data => {
-            createPic(data.message)
-        })
+            .then(resp => resp.json())
+            .then(data => {
+                createPic(data.message)
+                // event when key down
+                document.addEventListener('keydown', (e) => {
+                    if (e.key === "ArrowLeft") {
+                        //avoiding arrows to loop select menu
+                        e.preventDefault();
+                        pressedKey(changePic(data.message))
+                        
+                        console.log(changePic(data.message));
+                    }
+                    if (e.key === "ArrowRight") {
+                        //avoiding arrows to loop select menu
+                        e.preventDefault()
+                        console.log(changePic(data.message));
+                    }
+                })
+
+            })
     }
+
 }
 
+// !!!!! grab array of images to be changed when event is actioned !!!!!!
+function pressedKey(img) {
+    document.getElementById('pic').innerHTML = `
+    <div class="pic_pic" style="background-image: url('${img}');"></div>
+   
+    `
+}
+
+// generating pic on card
 function createPic(img) {
+
     document.getElementById('pic').innerHTML = `
     <div class="pic_pic" style="background-image: url('${img[0]}');"></div>
    
     `
-    addEventListener('keydown', () =>{
-        for (const i in img){
-            console.log(img[i])
-            
-        }
-    } )
+
+}
+
+// random pic generator
+function changePic(img) {
+    console.log(img[(Math.floor(Math.random() * img.length))])
+
 }
