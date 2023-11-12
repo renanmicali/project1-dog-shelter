9805794e9c93a3c426ea3cf09be51c3fbb44ca20
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-11-12 13:48:13 +1100

    nameList array added / html elemts added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-console.log("hello")
-
 const url = "https://dog.ceo/api/breeds/list/all"
 const list = document.getElementById('breed')
+
+const nameList = [
+	"Max","Buddy","Charlie","Jack","Cooper","Rocky","Toby","Tucker","Jake","Bear",
+	"Duke","Teddy","Oliver","Riley","Bailey","Bentley","Milo","Buster","Cody","Dexter",
+	"Winston","Murphy","Leo","Lucky","Oscar","Louie","Zeus","Henry","Sam","Harley",
+	"Baxter","Gus","Sammy","Jackson","Bruno","Diesel","Jax","Gizmo","Bandit","Rusty",
+	"Marley","Jasper","Brody","Roscoe","Hank","Otis","Bo","Joey","Beau","Ollie","Tank",
+	"Shadow","Peanut","Hunter","Scout","Blue","Rocco","Simba","Tyson","Ziggy","Boomer",
+	"Romeo","Apollo","Ace","Luke","Rex","Finn","Chance","Rudy","Loki","Moose","George",
+	"Samson","Coco","Benny","Thor","Rufus","Prince","Chester","Brutus","Scooter","Chico",
+	"Spike","Gunner","Sparky","Mickey","Kobe","Chase","Oreo","Frankie","Mac","Benji","Bubba",
+	"Champ","Brady","Elvis","Copper","Cash","Archie","Walter"
+]
 
 
 fetch(url)
@@ -79,3 +90,4 @@ function changePic(img) {
     return img[(Math.floor(Math.random() * img.length))]
 
 }
+
