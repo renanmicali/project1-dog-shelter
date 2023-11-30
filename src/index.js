7733cfb9285e5588e8009949430401b6c6a60c77
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-11-30 18:16:20 +1100

    changes pos project review / cleaning code

--- src/index.js
+++ src/index.js
@@ -10,21 +10,20 @@ fetch(url)
     .then(resp => resp.json())
     .then((data) => {
         createBreedList(data.message)
-
+        
     })
 // using map to show list and allow all breeds
 function createBreedList(breedList) {
     // !! 'this' will point to element in question when no other parameter can be used !!
     // using onchange event to select breed
-    // next step ------ > !!! instead of using onchange, try to use an eventListener function//
-
+   
     list.innerHTML = `
             <select onchange="loadBreed(this.value)" name="" id="opt">
             <option>Browse by Dog Breed</option>
-            ${Object.keys(breedList).map((breed) => {
+            ${Object.keys(breedList).map((breed) => { 
         return `<option>${breed}</option>`
-        // creates new strings when used in an array and separate them using ('')
-    }).join('')}
+        
+    })}
             </select>`
 }
 // londing images by breed
@@ -33,70 +32,59 @@ function loadBreed(breed) {
         fetch(`https://dog.ceo/api/breed/${breed}/images`)
             .then(resp => resp.json())
             .then(data => {
-                createPic(data.message)
-                // event when key down
-                document.addEventListener('keydown', (e) => {
-                    if (e.key === "ArrowLeft") {
-                        //avoiding arrows to loop select menu
-                        e.preventDefault();
-                        pressedKey(changePic(data.message))
-
-
-                    }
-                    if (e.key === "ArrowRight") {
-                        //avoiding arrows to loop select menu
-                        e.preventDefault()
-                        pressedKey(changePic(data.message));
-                    }
-                })
-
+                let imageArray = data.message
+                loadPic(data.message[0])
+                keyDownEvent(imageArray)               
             })
     }
 
 }
 
+function keyDownEvent(array) {
+    document.addEventListener('keydown', (e) => {
+        if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+            //avoiding arrows to loop select menu
+            e.preventDefault();
+            loadPic(changePic(array))
+        }
+      
+    })
+}
+
 // Last Listener added
-btn.addEventListener("click", generate);
 
 
-// !!!!! grab array of images to be changed when event is actioned !!!!!!
-function pressedKey(img) {
-    document.getElementById('pic').innerHTML = `
-    <div class="pic_pic" >
-        <img id="myImg" src="${img}" alt="">
-    </div>
-   
-    `
-}
-
-// generating pic on card
-function createPic(img) {
+function loadPic(imgUrl) {
 
     document.getElementById('pic').innerHTML = `
     <div class="pic_pic" >
-        <img id="myImg" src="${img[0]}" alt="">
+        <img id="myImg" src="${imgUrl}" alt="">
     </div>
    
-    `
-
-}
+    `}
 
 // random pic generator
 function changePic(img) {
-
     return img[(Math.floor(Math.random() * img.length))]
-
 }
 
+//give dog name
+
+btn.addEventListener("click", generate);
+
 function generate() {
-    if (output !== "Type Name Here" && true) {
+    if (output.value !== "Type Name Here" && output.value !== "") {
         output2.textContent = output.value
     } else {
         alert('Please give your Pet a name')
     }
-    // doesn't need to "return" anything. It just needs
-    // to set the value of the text field the provided value.
 };
+
+//clearing name
+
+function clearDogName(){
+    return output2.textContent = ""
+}
 
 
 ////////////////// !! modal !! ////////////////////////////////////
